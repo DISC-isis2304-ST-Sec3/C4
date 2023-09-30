0c3d4b9b6a9e27823e8ca7fefcbb6e9037fb848a
Juan Guevara <soyjuandavidguevara@gmail.com>
2023-09-29 22:48:17 -0500

    Html template

--- src/main/java/uniandes/edu/co/proyecto/controllers/ExamplesController.java
+++ src/main/java/uniandes/edu/co/proyecto/controllers/ExamplesController.java
@@ -1,36 +1,42 @@
 package uniandes.edu.co.proyecto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import uniandes.edu.co.proyecto.entities.ExampleEntity;
 import uniandes.edu.co.proyecto.repositories.ExampleRepository;
 
 import java.util.Collection;
 
-@RestController
+@Controller
 @RequestMapping("/examples")
 public class ExamplesController {
   @Autowired
   private ExampleRepository exampleRepository;
 
   @GetMapping
-  public Collection<ExampleEntity> getExamples() {
-    return exampleRepository.getExamples();
+  public String getExamples(Model model) {
+    Collection<ExampleEntity> examples = exampleRepository.getExamples();
+
+    model.addAttribute("examples", examples);
+
+    return "examples";
   }
 
-  @PostMapping
+  @PostMapping("/new/save")
   public String createExample(@ModelAttribute ExampleEntity example) {
     exampleRepository.createExample(example.getSomeValue());
-    return "redirect:index";
+    return "redirect:examples";
   }
 
-  @PutMapping("/{id}")
+  @PutMapping("/{id}/edit/save")
   public String updateExample(@PathVariable(name = "id") Long id, @ModelAttribute ExampleEntity example) {
     exampleRepository.updateExample(id, example.getSomeValue());
     return "redirect:index";
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/{id}/delete")
   public String deleteExample(@PathVariable(name = "id") Long id) {
     exampleRepository.deleteExample(id);
     return "redirect:index";
