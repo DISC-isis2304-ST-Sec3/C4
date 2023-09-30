db599802a85536e0526c588ddd2511f7b8387718
Juan Guevara <soyjuandavidguevara@gmail.com>
2023-09-29 22:20:03 -0500

    Insert feature

--- src/main/java/uniandes/edu/co/proyecto/controllers/ExamplesController.java
+++ src/main/java/uniandes/edu/co/proyecto/controllers/ExamplesController.java
@@ -1,9 +1,7 @@
 package uniandes.edu.co.proyecto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import uniandes.edu.co.proyecto.entities.ExampleEntity;
 import uniandes.edu.co.proyecto.repositories.ExampleRepository;
 
@@ -18,5 +16,11 @@ public class ExamplesController {
   @GetMapping
   public Collection<ExampleEntity> getExamples() {
     return exampleRepository.getExamples();
+  }
+
+  @PostMapping
+  public String createExample(@ModelAttribute ExampleEntity example) {
+    exampleRepository.createExample(example.getSomeValue());
+    return "redirect:index";
   }
 }
--- src/main/java/uniandes/edu/co/proyecto/repositories/ExampleRepository.java
+++ src/main/java/uniandes/edu/co/proyecto/repositories/ExampleRepository.java
@@ -1,7 +1,10 @@
 package uniandes.edu.co.proyecto.repositories;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import uniandes.edu.co.proyecto.entities.ExampleEntity;
 
 import java.util.Collection;
@@ -9,5 +12,10 @@ import java.util.Collection;
 public interface ExampleRepository extends JpaRepository<ExampleEntity, Long> {
 
   @Query(value = "select * from examples", nativeQuery = true)
-  public Collection<ExampleEntity> getExamples();
+  Collection<ExampleEntity> getExamples();
+
+  @Modifying
+  @Transactional
+  @Query(value = "insert into examples (id, some_value) values (examples_sequence.nextval, :someValue)", nativeQuery = true)
+  void createExample(@Param("someValue") String someValue);
 }
