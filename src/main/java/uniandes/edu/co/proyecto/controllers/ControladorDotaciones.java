5999568afcafa5b84682069ff589bfc909e2fee7
daniela <d.parram2@uniandes.edu.co>
2023-10-01 14:20:57 -0500

    Dotaciones Completo

---
+++ src/main/java/uniandes/edu/co/proyecto/controllers/ControladorDotaciones.java
@@ -0,0 +1,52 @@
+package uniandes.edu.co.proyecto.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import uniandes.edu.co.proyecto.entities.Dotacion;
+import uniandes.edu.co.proyecto.repositories.DotacionesRepositorio;
+
+
+@Controller
+@RequestMapping("/dotaciones")
+public class ControladorDotaciones {
+    @Autowired
+  private DotacionesRepositorio dotacionesRepositorio;
+
+  @GetMapping
+  public String obtenerDotaciones(Model model) {
+    model.addAttribute("dotaciones", dotacionesRepositorio.obtenerDotaciones());
+
+    return "dotaciones";
+  }
+
+  @PostMapping("/new/save")
+  public String crearDotacion(@ModelAttribute Dotacion dotacion) {
+    dotacionesRepositorio.crearDotacion(dotacion.getNombre(), dotacion.getCantidad(), dotacion.getCostoAdicional());
+
+    return "redirect:/dotaciones";
+  }
+
+  @GetMapping("/{id}/edit")
+  public String actualizarDotacionesForm(@PathVariable(name = "iddotacion") long iddotacion, Model model) {
+
+    model.addAttribute("dotaciones", dotacionesRepositorio.obtenerDotacion(iddotacion));
+
+    return "editarDotaciones";
+  }
+
+  @PostMapping("/{id}/edit/save")
+  public String actualizarDotaciones(@PathVariable(name = "iddotacion") long iddotacion, @ModelAttribute Dotacion dotacion) {
+    dotacionesRepositorio.actualizarDotacion(iddotacion, dotacion.getNombre(), dotacion.getCantidad(), dotacion.getCostoAdicional());
+
+    return "redirect:/dotaciones";
+  }
+
+  @GetMapping("/{id}/delete")
+  public String eliminarDotacion(@PathVariable(name = "iddotacion") long iddotacion) {
+    dotacionesRepositorio.eliminarDotacion(iddotacion);
+
+    return "redirect:/dotaciones";
+  }
+}
