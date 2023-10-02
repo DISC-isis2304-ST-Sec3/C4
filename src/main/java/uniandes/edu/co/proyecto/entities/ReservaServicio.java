b318cf45ba59608497e3829090e12186680bc9b4
Juan Guevara <soyjuandavidguevara@gmail.com>
2023-10-01 21:31:31 -0500

    Repo reservas

--- src/main/java/uniandes/edu/co/proyecto/entities/ReservaServicio.java
+++ src/main/java/uniandes/edu/co/proyecto/entities/ReservaServicio.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.util.Date;
 
 @Entity
+@Table(name = "reservas_servicio")
 public class ReservaServicio {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,4 +17,49 @@ public class ReservaServicio {
   @ManyToOne
   @JoinColumn(name = "id_salon", referencedColumnName = "id")
   private Salon salon;
+
+  @ManyToOne
+  @JoinColumn(name = "id_spa", referencedColumnName = "id")
+  private Spa spa;
+
+  public ReservaServicio() {
+  }
+
+  public ReservaServicio(Date horario, Salon salon, Spa spa) {
+    this.horario = horario;
+    this.salon = salon;
+    this.spa = spa;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Date getHorario() {
+    return horario;
+  }
+
+  public void setHorario(Date horario) {
+    this.horario = horario;
+  }
+
+  public Salon getSalon() {
+    return salon;
+  }
+
+  public void setSalon(Salon salon) {
+    this.salon = salon;
+  }
+
+  public Spa getSpa() {
+    return spa;
+  }
+
+  public void setSpa(Spa spa) {
+    this.spa = spa;
+  }
 }
