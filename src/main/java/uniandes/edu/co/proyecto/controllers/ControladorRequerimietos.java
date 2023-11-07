acd0e2246e9dfd32d437dc9cad17839cca4a117c
Juan Guevara <soyjuandavidguevara@gmail.com>
2023-11-07 14:56:53 -0500

    Finished req 3 UI

--- src/main/java/uniandes/edu/co/proyecto/controllers/ControladorRequerimietos.java
+++ src/main/java/uniandes/edu/co/proyecto/controllers/ControladorRequerimietos.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import uniandes.edu.co.proyecto.entities.especiales.DineroHabitacion;
 import uniandes.edu.co.proyecto.repositories.iteracion2.RepositorioReq1;
 import uniandes.edu.co.proyecto.repositories.iteracion2.RepositorioReq2;
+import uniandes.edu.co.proyecto.repositories.iteracion2.RepositorioReq3;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,6 +19,8 @@ public class ControladorRequerimietos {
   private RepositorioReq1 repositorioReq1;
   @Autowired
   private RepositorioReq2 repositorioReq2;
+  @Autowired
+  private RepositorioReq3 repositorioReq3;
 
   private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
@@ -52,23 +55,30 @@ public class ControladorRequerimietos {
   @GetMapping("/servicios_populares/{date1}/{date2}")
   public String obtenerServiciosMasPopularesFechas(
     Model model,
-    @PathVariable(name = "date1", required = true) String date1,
-    @PathVariable(name = "date2", required = true) String date2
+    @PathVariable(name = "date1") String date1,
+    @PathVariable(name = "date2") String date2
   ) throws ParseException {
     Date fechaInicial = dateFormat.parse(date1);
     Date fechaFinal = dateFormat.parse(date2);
 
-//    List<Map<String, Object>> r = especialesRepositorio.ejecutarRequerimiento2(fechaInicial, fechaFinal);
-//    for (Map<String, Object> m: r) {
-//      for (Map.Entry<String, Object> entry : m.entrySet()) {
-//        System.out.printf("%s, %s\n", entry.getKey(), entry.getValue());
-//      }
-//      System.out.println("=");
-//    }
     model.addAttribute("resultados", repositorioReq2.ejecutarRequerimiento2(fechaInicial, fechaFinal));
     model.addAttribute("fechaInicial", date1);
     model.addAttribute("fechaFinal", date2);
 
     return "servicios_populares";
+  }
+
+  @GetMapping("/ocupacion_habitaciones")
+  public String obtenerOcupacionHabitacionesHotelUltimoAnio(
+      Model model,
+      @RequestParam(name = "hotel", defaultValue = "-1") long idHotel
+  ) {
+    if (idHotel == -1) {
+      model.addAttribute("resultados", new ArrayList<>());
+    } else {
+      model.addAttribute("resultados", repositorioReq3.ejecutarRequerimiento3(idHotel));
+    }
+
+    return "ocupacion_habitaciones";
   }
 }
